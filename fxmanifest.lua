41f4e7b182696917fbb518fabfd279c147dd0b43
skyzen <155313718+0skyzen@users.noreply.github.com>
2025-12-24 19:21:12 +0100

    Refactor fxmanifest.lua to simplify scripts

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,39 +1,19 @@
-shared_script '@WaveShield/resource/include.lua'
-
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 version '1.0.0'
 
 shared_scripts {
-	'@ox_lib/init.lua',
-	'config/config_license.lua',
-	'config/config_scanner.lua',
-	'config/config_jail.lua',
-	'shared/shared.lua'
+	'@ox_lib/init.lua'
 }
 
 server_scripts {
-	'@es_extended/imports.lua',
-	'@oxmysql/lib/MySQL.lua',
-	'@mysql-async/lib/MySQL.lua',
-	'server/license.lua',
-	'server/scanner.lua',
-	'server/pdprops.lua',
-	'server/jail.lua'
+	'sv_pdprops.lua'
 }
 
 client_scripts {
-	'client/vhsText.lua',
-	'client/scanner/*.lua',
-	'client/pdprops/*.lua',
-	'client/jail/*.lua'
+	'cl_pdprops.lua'
 }
 
-ui_page 'ui/index.html'
 
-files {
-	'ui/**',
-	'nui/**'
-}
 
